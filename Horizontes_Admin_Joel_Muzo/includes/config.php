df90982ea8aaf733d2eee6902521dfc8f133eb40
JoelMuzo23 <134490059+JoelMuzo23@users.noreply.github.com>
2024-05-28 23:36:31 +0200

    Add files via upload

---
+++ Horizontes_Admin_Joel_Muzo/includes/config.php
@@ -0,0 +1,5 @@
+<?php
+    const BBDD_USER="joel1";
+    const BBDD_PASSWORD="1234";
+    const DSN = "mysql:host=joel-bbdd.duckdns.org; dbname=Proyecto; charset=utf8";
+?>
---
+++ Horizontes_Admin_Joel_Muzo/includes/footer.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+    <!-- Parte Inferior de la pagina -->
+   <div class="row mt-3">
+        <div class="col-md-12">
+
+            <!-- Caja donde va ha estar el autor de la pagina-->
+            <div class="caja">
+
+                <!-- Caja donde van a estar los botones de las redes sociales -->
+                <div>
+                    <a href="#" class="icono"><i class="fab fa-instagram"></i></a>
+                    <a href="#" class="icono"><i class="fab fa-facebook"></i></a>
+                    <a href="#" class="icono"><i class="fab fa-twitter"></i></a>
+                </div>
+                <p>@JoelMuzo - I♥</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
