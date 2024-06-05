c75c7e4725b1553e38daaeaf1f8c0311042ac788
JoelMuzo23 <134490059+JoelMuzo23@users.noreply.github.com>
2024-06-05 19:06:13 +0200

    Add files via upload

---
+++ Horizontes_Admin_Joel_Muzo/includes/header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+</head>
+<body>
+    <!-- Aqui se muestra una barra de navegacion en el cual incluye un logo y un boton para volver al menu del administrador -->
+  <nav class="navbar navbar-expand-lg bg-black sticky-top">
+      <div class="container-fluid">
+          <img class="logo" src="../../../img/Logo.png" alt="">
+          <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse justify-content-end ml-3" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+              <li class="nav-item">
+                <div class="boton"> <a class="nav-link" aria-current="page" href="../AdminMenu.php">Volver</a> </div>
+              </li>
+            </ul>
+          </div> 
+      </div>
+  </nav>
+</body>
+</html>
